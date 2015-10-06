f58bcd0420da30f81f329abf0cfc2acd38e513a5
perlancar (@pc-office) <perlancar@gmail.com>
2015-10-06 19:49:02 +0700

    Initial commit

---
+++ lib/App/ListRedhatReleases.pm
@@ -0,0 +1,13 @@
+package App::ListRedhatReleases;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: List RedHat releases (CLI)
+
+=head1 DESCRIPTION
+
+See the included script L<list-redhat-releases>.
+
+=cut
